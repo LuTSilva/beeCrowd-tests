09cee0a83ae89392422bfa4cfabef19a65e929b1
Luan <luantome.sc@gmail.com>
2025-03-05 21:43:35 -0300

    Soluções Bee 1003, 1004 e 1019

---
+++ Bee-1019/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// Leitura do tempo, em segundos
+        System.out.print("Digite o tempo em segundos: ");
+        int N = sc.nextInt();
+		
+        // Cálculo do tempo em horas, minutos e segundos
+		int horas = N / 3600;
+		int minutos = (N % 3600) / 60;
+        int segundos = N % 60;
+		
+        // Impressão do tempo em horas, minutos e segundos
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+		
+		sc.close();
+	}
+}
